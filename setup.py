dc9a182f4e944309b02a597e07d06f7c27514669
jihopark <lastspurt17@gmail.com>
2017-09-21 16:34:00 +0800

    trying to upload to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hltc_preprocess',
+  packages = ['hltc_preprocess'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Text Preprocessing scripts for lab use',
+  author = 'Ji Ho Park',
+  author_email = 'jhpark@connect.ust.hk',
+  url = 'https://github.com/jihopark/hltc_preprocess', # use the URL to the github repo
+  download_url = 'https://github.com/jihopark/hltc_preprocess/archive/0.1.tar.gz', 
+  keywords = ['nlp', 'preprocessing'], # arbitrary keywords
+  classifiers = [],
+)
