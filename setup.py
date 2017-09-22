abc64249ce1b917a31fdc890a67c674744af355d
jihopark <lastspurt17@gmail.com>
2017-09-22 10:42:31 +0800

    gone through some mistakes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'hltc_preprocess',
   packages = ['hltc_preprocess'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.5',
   description = 'Text Preprocessing scripts for lab use',
   author = 'Ji Ho Park',
   author_email = 'jhpark@connect.ust.hk',
   url = 'https://github.com/jihopark/hltc_preprocess', # use the URL to the github repo
-  download_url = 'https://github.com/jihopark/hltc_preprocess/archive/0.4.tar.gz', 
+  download_url = 'https://github.com/jihopark/hltc_preprocess/archive/0.5.tar.gz', 
   keywords = ['nlp', 'preprocessing'], # arbitrary keywords
   classifiers = [],
 )
